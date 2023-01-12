d7e59175af88d4592371cf1d347db0cd7240b893
Gabrieltrinidad0101 <gabrielqwes123@gmail.com>
2023-01-11 20:28:37 -0400

    end app

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+
+function App() {
+  const [data,setData] = useState({holaMundo: ""});
+  const getDataFromApi = async _=>{
+    try{
+      const res = await fetch("http://localhost:3000")
+      const info = await res.json();
+      setData(info)
+    }catch{
+      setData({holaMundo: "Error in the api"})
+    }
+  }
+  useEffect(_=>{
+    getDataFromApi();
+  },[])
+  return (
+    <div className="App">
+      {
+        data.holaMundo
+      }
+    </div>
+  )
+}
+
+export default App
